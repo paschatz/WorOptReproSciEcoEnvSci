3055352eccb667ec52013bc7d2cfab244b65e5c7
Paschalis Chatzopoulos <58082380+paschatz@users.noreply.github.com>
2023-10-03 17:21:41 +0300

    add script and raw data
    This is a dataset with participaitng sites for a synthesis project I am working on.

---
+++ scripts/participatin_sites_map.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+library(maps)
+library(tidyverse)
+library(readxl)
+library(leaflet)
+
+# Load file with coordinates
+file <- read_xlsx("raw_data/participating_experiments.xlsx", # file path
+                  sheet = 2, # sheet number
+                  na = "NA") # read_xlsx has an additional argument for NAs
+
+file$longitude <- as.numeric(file$longitude)
+file$latitude <- as.numeric(file$latitude)
+str(file)
+coord <- file %>% 
+  select(latitude, longitude)
+
+# Plot
+world_map <- ggplot() +
+  borders("world", fill = "white", colour = "red") +
+  geom_point(data = file, aes(longitude, latitude), size = 2) +
+  coord_fixed(1.3) +
+  theme_void() +
+  theme(legend.position = "none")
+
+# Display map with ggplot
+print(world_map)
+
+# Display map with leaflet
+map <- leaflet(coord) %>%
+  addTiles() %>%
+  addMarkers()
+map
